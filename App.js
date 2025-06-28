ec2daa3faed33059c24fa485d3d3846ea2e113b1
HNI <dh505jaat@gmail.com>
2025-06-28 15:30:05 +0530

    src
    App.js
    index.css
    index.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', fontSize: '24px', textAlign: 'center' }}>
+      🎮 Welcome to Ginger Activities! Your Game Hub 🚴‍♂️⚡
+    </div>
+  );
+}
+
+export default App;
